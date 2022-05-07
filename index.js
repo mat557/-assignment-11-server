6450d6fe3c7d3d547ccd82bfbe51b756027c2ee3
mat557 <matasnim11@gmail.com>
2022-05-07 23:39:21 +0600

    delete done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const res = require('express/lib/response');
 const port = process.env.PORT || 5000;
 const app = express();
@@ -22,10 +22,39 @@ async function run(){
         app.get('/services',async (req,res)=>{
             const query = {};
             const cursor = serviceCollection.find(query);
-            const service = await cursor.toArray();
-            res.send(service);
+            const services = await cursor.toArray();
+            res.send(services);
         });
 
+
+        app.get('/services/:id',async (req,res)=>{
+            const id = req.params.id;
+            // console.log(id);
+            const query = {_id : ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+
+        app.delete('/services/:id',async (req,res)=>{
+            const id = req.params.id;
+            const query = {_id : ObjectId(id)};
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // app.put('/services/:id',async(req,res)=>{
+        //     const id = req.params.id;
+        //     const updateQuantity = req.body;
+        //     const filter = { _id: ObjectId(id) };
+        //     const options = { upsert: true };
+        //     const updateDoc = {
+        //         $set: {
+                  
+        //         },
+        //       };
+        //     const result = await serviceCollection.updateOne(filter, updateDoc, options);
+        //     res.send(result);
+        // })
 
 
     }
